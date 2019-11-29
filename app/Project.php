f72fe56515279693bb3a8377e41ff2768ca173b9
Suparerk <ch.suparerk@gmail.com>
2019-11-30 06:25:05 +0700

    Eloquent relationships

--- app/Project.php
+++ app/Project.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = [
-        'title', 'description'
-    ];
+    // protected $fillable = [
+    //     'title', 'description'
+    // ];
+
+    protected $guarded = [];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
