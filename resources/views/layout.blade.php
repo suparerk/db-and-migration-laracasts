c08f0222412ae0946ce21bc5e6ceea9056e49577
Suparerk <ch.suparerk@gmail.com>
2019-11-30 06:54:20 +0700

    Form Action Considerations

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+  <style>
+    .is-complete {
+      text-decoration: line-through;
+    }
+  </style>
   <title> @yield('title', 'Projects')</title>
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Route::get('/', function () {
 });
 
 Route::resource('projects', 'ProjectsController');
+
+Route::patch('/tasks/{task}', 'ProjectTasksController@update');
