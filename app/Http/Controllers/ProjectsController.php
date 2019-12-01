fd186a446da88a5b68672a2641f70d36b8d3290f
Suparerk <ch.suparerk@gmail.com>
2019-12-02 06:01:06 +0700

    Refactor controller for more readability

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -17,11 +17,12 @@ class ProjectsController extends Controller
 
     public function index()
     {
-        $projects = Project::where('owner_id', auth()->id())->get();
+        $projects = auth()->user()->projects;
+        // $projects = Project::where('owner_id', auth()->id())->get();
 
-        dump($projects);
         return view('projects.index', compact('projects'));
     }
+
     public function show(Project $project)
     {
         // abort_if()
@@ -32,6 +33,7 @@ class ProjectsController extends Controller
 
         return view('projects.show', compact('project'));
     }
+
     public function create()
     {
         return view('projects.create');
@@ -39,33 +41,41 @@ class ProjectsController extends Controller
 
     public function store()
     {
-        $validated_attributes =request()->validate([
-            'title' => ['required', 'min:3'],
-            'description' => ['required', 'min:3']
-        ]);
-
+        $validated_attributes = $this->validateProject();
         $validated_attributes['owner_id'] = auth()->id();
-        $project = Project::create($validated_attributes);
+        $project = Project::create(validated_attributes);
 
         \Mail::to('ch.suparerk@gmail.com')->send(
             new ProjectCreated($project)
         );
         return redirect('/projects');
     }
+
     public function update(Project $project)
     {
-        Project::update(request(['title', 'description']));
+        $project->update($this->validateProject());
 
         return redirect('/projects');
     }
+
     public function edit(Project $project)
     {
         return view('projects.edit', compact('project'));
     }
+
     public function destroy(Project $project)
     {
         $project->delete();
 
         return redirect('/projects');
+    }
+
+    protected function validateProject()
+    {
+        return request()->validate([
+            'title' => ['required', 'min:3'],
+            'description' => ['required', 'min:3']
+        ]);
+        
     }
 }
--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -28,6 +28,8 @@
     </div>
   </form>
 
+  @include('errors')
+
   <form method="POST" action="/projects/{{ $project->id }}">
     @method('DELETE')
     @csrf
